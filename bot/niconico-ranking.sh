b96d98401868cdbf888c3b423d3c986aeb25d775
unlimish <mail@unlimish.xyz>
2020-07-06 21:53:28 +0900

    🔧Add: output hourly ranking xml file

--- bot/niconico-ranking.sh
+++ bot/niconico-ranking.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 dirname="/home/oga/workspace/niconico-ranking-rss"
 mkdir -p $dirname
+filename="${dirname}/hourly-ranking-`date + '%Y%m%d%H%M'`.xml"
+echo "Save to $filename"
+curl -s -o $filename -H "User-Agent: CrawBot; mail@unlimish.xyz" https://www.nicovideo.jp/ranking/genre/all?rss=2.0&lang=ja-jp
